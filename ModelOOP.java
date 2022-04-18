50c7cc146c73b843136dab7f023ab91e576a64ca
harnowo06 <102216069+harnowo06@users.noreply.github.com>
2022-04-18 14:18:16 +0700

    Add files via upload

---
+++ ModelOOP.java
@@ -0,0 +1,28 @@
+public class ModelOOP() {
+    public static void main(String[] args) {
+        
+    }
+     //public method
+     public void nama() {
+         System.out.println("icon like");
+         
+     }
+
+     public void nama() {
+      System.out.println("icon comment"); 
+      
+    }
+
+    public void nama() {
+        System.out.println("icon direct message");
+    }
+
+    public void nama() {
+        System.out.println("Jumlah Likes");
+    }
+
+    public void nama() {
+        System.out.println("kolom komentar dari akun lain");
+    }
+
+}
---
+++ data matrix.java
@@ -0,0 +1,30 @@
+public class data matrix{
+
+    static class graph{
+        int vertices;
+        int matrix[][];
+    }
+
+public graph(int data matrix) {
+    this.vertices = data matrix;
+
+    matrix = new int[data matrix] [data matrix];
+    
+    }
+
+    public void addedge(int Likes , int comment , int share) {
+        //addegde
+        matrix [source][destination]=share;
+
+        //add back edge for undirected graph
+        matrix [destination][source]=share;
+        
+    }
+
+    //get the data matrix with minimum distance which is not included in SPT
+
+    int get minimum data matrix
+
+
+
+}
